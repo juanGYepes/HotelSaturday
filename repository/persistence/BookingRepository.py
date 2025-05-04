a38d6f37247ecd09baa7e375f315da8e362b7c6b
ValenAgui <Valentinaaguirre067@gmail.comgit>
2025-05-04 14:58:35 -0500

    agrego validacion a booking repository

--- repository/persistence/BookingRepository.py
+++ repository/persistence/BookingRepository.py
@@ -6,7 +6,20 @@ class BookingRepository:
         self.booking = Booking
 
     def create_booking_repository(self, booking, db):
-        query = "INSERT INTO booking (id, booking_date, checkout_date, guest_id, bedroom_id, main_service_id, optional_service_id, employee_id)"
+        required_fields = [booking.id, booking.booking_date, booking.checkout_date,
+                           booking.guest.id, booking.bedroom.id, booking.main_service.id,
+                           booking.optional_service.id, booking.employee.id]
+
+        if not all(required_fields):
+            print("🚨 Todos los campos de reserva son obligatorios.")
+            return
+
+        query = """
+            INSERT INTO booking (
+                id, booking_date, checkout_date, guest_id, bedroom_id,
+                main_service_id, optional_service_id, employee_id
+            ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """
         values = (
             booking.id,
             booking.booking_date,
@@ -19,4 +32,3 @@ class BookingRepository:
         )
 
         db.execute_query(query, values)
-
